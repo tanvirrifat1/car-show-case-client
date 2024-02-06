94cfb814b99c6e25d88cd107b61a23306bf740e1
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2024-02-06 19:31:12 +0600

    added redux rtk

--- src/lib/Provider.tsx
+++ src/lib/Provider.tsx
@@ -6,13 +6,14 @@ import { Provider } from "react-redux";
 import { NextUIProvider } from "@nextui-org/react";
 import { Toaster } from "react-hot-toast";
 import { IChildrenProps } from "@/types/common";
+import { store } from "@/redux/store";
 
 const Providers = ({ children }: { children: IChildrenProps }) => {
   return (
-    <>
+    <Provider store={store}>
       <NextUIProvider>{children}</NextUIProvider>
       <Toaster position="top-center" reverseOrder={false} />
-    </>
+    </Provider>
   );
 };
 
